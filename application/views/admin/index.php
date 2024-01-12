72e5beaf163cc19542996b66610572696c917de1
HekalG75 <haikalmuhamad024@gmail.com>
2024-01-12 15:54:42 +0700

    tes

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -15,7 +15,7 @@
                <div class="card-body">
                  <div class="row no-gutters align-items-center">
                    <div class="col mr-2">
-                     <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Anjay</div>
+                     <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Department</div>
                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $display['c_department']; ?> Department</div>
                    </div>
                    <div class="col-auto">
